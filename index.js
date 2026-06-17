82a48fd0f51fb40ace89512046d81e616a4be77a
moti7846 <8563816@gmail.com>
2026-06-18 02:37:21 +0300

    re-add dotenv configuration import in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const FormData = require('form-data');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
